5e8b44f6d450803da1ad94ef233ef3e79810dd7e
Boaz <boazarad88@gmail.com>
2015-06-06 19:57:13 +0300

    Server library UT minor update

--- library-server/src/test/java/il/ac/technion/cs/sd/lib/ServerTest.java
+++ library-server/src/test/java/il/ac/technion/cs/sd/lib/ServerTest.java
@@ -7,6 +7,7 @@ import il.ac.technion.cs.sd.msg.MessengerException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class ServerTest {
 	
@@ -15,6 +16,10 @@ public class ServerTest {
 			super(serverName);
 		}
 		
+		public FakeServer(String serverName, Messenger messenger) {
+			super(serverName,messenger);
+        }
+
 		@Override public MessageWrapper handleMessage(MessageWrapper msgWrapper) throws MessengerException {
 	        return msgWrapper;
         }
@@ -22,21 +27,32 @@ public class ServerTest {
 	
 	private final String serverName = "server";
 	private FakeServer server;
+	private Messenger messenger;
 	private final String to = "someone";
 	private final String from = "client";
 	private final String data = "something";
 	private final int type = 1;
 	private final MessageWrapper m = new MessageWrapper(to,from, data, type);
+	private final String j = JsonAuxiliary.messageWrapperToJson(m);
 	
 	@Before
 	public void setUp() throws Exception {
-		server = new FakeServer(serverName);
+		messenger = Mockito.mock(Messenger.class);
+		Mockito.when(messenger.getAddress()).thenReturn(serverName);
+		Mockito.doNothing().when(messenger).send(Mockito.anyString(),Mockito.anyString());
+		Mockito.when(messenger.getNextMessage(Mockito.anyLong())).thenReturn("");
+		server = new FakeServer(serverName, messenger);
 	}
 	
 	@After
 	public void tearDown() throws Exception {
 		server.kill();
 		server = null;
+	}
+	
+	@Test
+	public void getServerNameReturnsTheServersName() throws Exception {
+		assertEquals(server.getAddress(),serverName);
 	}
 	
 	@Test public void test() {
